35831375b5b08b123642e02aa54bb7654fc83ebd
Muiz Hensem <mmuizzuddin123@gmail.com>
2025-07-07 22:52:20 +0800

    Remove Auth Check

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
- 
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,19 +11,13 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"></script>
 </head>
- 
+
 <body>
     <section class="bg-gray-500 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 Jom Sewa
             </div>
-            <?php
-            use Illuminate\Support\Facades\Auth;
-
-            //dd(Auth::check(), Auth::user());
-            
-            ?>
             <img src="{{ asset('img/logo.png') }}" style="width: 100px" alt="">
             <br>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -78,5 +72,5 @@
         </div>
     </section>
 </body>
- 
+
 </html>
